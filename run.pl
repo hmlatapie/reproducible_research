f6623fb7e76ac0fc8e39bf6133420a08d1baf35b
hugo latapie <hmlatapie@yahoo.com>
2016-05-10 22:51:19 -0700

    run.pl now supports nvidia-docker

--- run.pl
+++ run.pl
@@ -11,15 +11,19 @@ $options{RR_CONTAINER} = $ENV{RR_CONTAINER}
 $usage = <<EOS;
 $0 cmd options
 	valid commands:
-		start name 
+		start name [docker_repo]
+			docker_repo
+				defaults to $options{RR_CONTAINER}
 			--pull
 				will pull latest
+			--nvidia
+				run via nvidia-docker
+			--nomongo
+				don't create mongodb
 		test name 
 	options:
 		--defaultdb=
 			defaults to $options{defaultdb}
-		--RR_CONTAINER
-			defaults to $options{RR_CONTAINER}
 EOS
 
 GetOptions(
@@ -27,6 +31,8 @@ GetOptions(
 	'RR_CONTAINER=s' => \$options{RR_CONTAINER},
 	'booleanparam' => \$options{booleanparam},
 	'pull' => \$options{pull},
+	'nvidia' => \$options{nvidia},
+	'nomongo' => \$options{nomongo},
 	);
 
 confess $usage
@@ -37,8 +43,15 @@ $command = shift;
 if($command eq 'start')
 {
 	$name = shift;
+	$docker_repo = shift;
 	confess $usage
 		if !$name;
+
+	$options{RR_CONTAINER} = $docker_repo
+		if $docker_repo;
+
+	$options{nvidia} = 1
+		if $options{RR_CONTAINER} =~ /(hmlatapie\/rr_caffe)/;
 
 	$mongodb = "$name\_mongo";
 
@@ -48,18 +61,26 @@ if($command eq 'start')
 docker pull $options{RR_CONTAINER} 
 EOS
 
-	execute($cmd);
+	execute($cmd)
+		if $options{pull};
+
 	execute('docker pull hmlatapie/mongodb')
 		if $options{pull};
 
 	execute('docker pull $options{RR_CONTAINER}')
 		if $options{pull};
 
-	execute("docker run -d --name $mongodb -e AUTH=no hmlatapie/mongodb");
+	execute("docker run -d --name $mongodb -e AUTH=no hmlatapie/mongodb")
+		if !$options{nomongo};
+
+	$dockercmd = 'docker';
+	$dockercmd = 'nvidia-docker'
+		if $options{nvidia};
 
 	$cmd =<<EOS;
-docker run -d --name $name --link $mongodb:mongodb --volume=\$(pwd)/rr:/root/rr $options{RR_CONTAINER} /bin/bash -c "while true; do date; sleep 3600; done"
+$dockercmd run -d --name $name --link $mongodb:mongodb --volume=\$(pwd)/rr:/root/rr $options{RR_CONTAINER} /bin/bash -c "while true; do date; sleep 3600; done"
 EOS
+
 	execute($cmd);
 }
 elsif($command eq 'test')
